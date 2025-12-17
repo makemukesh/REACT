cfdac6fa328af018887ad0213839e6d75ab978e7
makemukesh <makemukesh2006@gmail.com>
2025-12-17 15:13:38 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+
+
+
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:- http://localhost:${PORT}`);
+});
