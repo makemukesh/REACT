4e41d915f73bd1eaaadcb17fc8e67091ff58e65c
makemukesh <makemukesh2006@gmail.com>
2025-12-27 13:18:40 +0530

    Fix: Set default port to 10000 and bind 0.0.0.0 for Render

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use('/api/admin/cars', carRoutes);
 app.use('/users', userRoutes);
 app.use('/api/admin/products', productRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
   res.send('Backend  is running...');
