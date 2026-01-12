818397e0001359f51ed9951ed1f117574576c6d0
makemukesh <makemukesh2006@gmail.com>
2026-01-12 12:24:05 +0530

    Fixed the errors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: 'https://react-liard-gamma.vercel.app',
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://react-liard-gamma.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
